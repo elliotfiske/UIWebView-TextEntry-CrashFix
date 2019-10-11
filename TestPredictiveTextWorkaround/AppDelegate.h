01e7f09cd562dfb13d986653795db2a495ca4fbf
Elliot Fiske <elliot.fiske@salesforce.com>
2019-10-11 14:18:18 -0700

    Initial Commit

---
+++ TestPredictiveTextWorkaround/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestPredictiveTextWorkaround
+//
+//  Created by Elliot Fiske on 10/11/19.
+//  Copyright © 2019 elliotfiske. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestPredictiveTextWorkaround/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPredictiveTextWorkaround
+//
+//  Created by Elliot Fiske on 10/11/19.
+//  Copyright © 2019 elliotfiske. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
